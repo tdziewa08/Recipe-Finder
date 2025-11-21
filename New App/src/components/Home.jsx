8d3845e17109bbd131d0b9465ab9baeb27f6767a
tdziewa08 <141276940+tdziewa08@users.noreply.github.com>
2025-11-21 16:38:37 -0500

    more styling and rough button functionality

--- New App/src/components/Home.jsx
+++ New App/src/components/Home.jsx
@@ -1,30 +1,72 @@
+import React from "react"
+import IngredientList from "./IngredientList"
+
 export default function Home() {
+
+    const [myIngredients, setMyIngredients] = React.useState([])
+    const [ingredientInput, setIngredientInput] = React.useState("")
+    const [amountInput, setAmounttInput] = React.useState("")
+    const [selectedUnit, setSelectedUnit] = React.useState("")
+
+    function addItem() {
+        if(ingredientInput || amountInput)
+        {
+            let newestItem = {}
+            newestItem = {
+                ingredient: ingredientInput,
+                amount: amountInput,
+                unit: selectedUnit    
+            }
+            setMyIngredients(prevArray => [...prevArray, newestItem])
+            setIngredientInput("")
+            setAmounttInput("")
+            setSelectedUnit("")
+        }
+        
+    }
+
+    console.log(myIngredients)
+
     return (
-        <div className="home-container">
-            <h1>Recipe Optimitzer</h1>
-            <p className="app-desc">Let us know what ingredients you have, and we'll generate the best recipe we can to fit YOUR goals!</p>
-            <div className="ingredient-input-container">
-                <div className="input-section">
-                    <div className="ingredient-input">
-                        <span>Ingredient</span>
-                        <input type="text" placeholder="E.g Broccoli"></input>
+        <>
+            <div className="home-container">
+                <h1>Recipe Optimitzer</h1>
+                <p className="app-desc">Let us know what ingredients you have, and we'll generate the best recipe we can to fit YOUR goals!</p>
+                <div className="ingredient-input-container">
+                    <div className="input-section">
+                        <div className="ingredient-input">
+                            <span>Ingredient</span>
+                            <input
+                                value={ingredientInput}
+                                onChange={(e) => setIngredientInput(e.target.value)}
+                                type="text"
+                                placeholder="E.g Broccoli"
+                            />
+                        </div>
+                        <div className="ingredient-input">
+                            <span>Amount</span>
+                            <input
+                                value={amountInput}
+                                onChange={(e) => setAmounttInput(e.target.value)}
+                                type="text"
+                                placeholder="E.g 4"
+                            />
+                            <select value={selectedUnit} onChange={(e) => setSelectedUnit(e.target.value)}>
+                                <option>cups</option>
+                                <option>ounces</option>
+                                <option>fluid ounces</option>
+                                <option>grams</option>
+                                <option>pounds</option>
+                                <option>units</option>
+                            </select>
+                        </div>
                     </div>
-                    <div className="ingredient-input">
-                        <span>Amount</span>
-                        <input type="text" placeholder="E.g 4"></input>
-                        <select>
-                            <option>cups</option>
-                            <option>ounces</option>
-                            <option>fluid ounces</option>
-                            <option>grams</option>
-                            <option>pounds</option>
-                            <option>gallons</option>
-                        </select>
-                    </div>
+                    <button onClick={addItem}>Add Item</button>
                 </div>
-                
-                <button>Add Item</button>
             </div>
-        </div>
+            <IngredientList
+                list={myIngredients}
+            />
+        </>
     )
 }
--- New App/src/components/IngredientList.jsx
+++ New App/src/components/IngredientList.jsx
@@ -1,12 +1,20 @@
-export default function IngredientList() {
+export default function IngredientList(props) {
 
 const fakeIngredients = ["apples", "bananas", "butter", "ravioli", "ground beef", "vanilla extract", "ground beef", "vanilla extract"]
-const theIngredients = fakeIngredients.map(item => {
-    return <span>{item} <button>X</button></span>
+const theIngredients = props.list.map(item => {
+    return (
+        <p>
+            <span>{item.ingredient}</span>
+            <span>{item.amount}</span>
+            <span>{item.unit}</span>
+            <button>X</button>
+        </p>
+    )
 })
 
     return (
         <div className="ingredient-list-container">
+            <h2>Your Ingredients</h2>
             {theIngredients}
         </div>
     )
